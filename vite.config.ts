c526dcd5f00af4143f01e4d01b1abae934aa8cfa
Trent Fridey <tfri.92@gmail.com>
2022-09-17 00:44:55 -0500

    use gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/wordle-clone/',
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   define: { "global": {}},
 })
