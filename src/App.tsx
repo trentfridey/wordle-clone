e5f21eed8bc4eca86b0f850a6629fa89ec84b774
trentfridey <tfri.92@gmail.com>
2023-02-27 11:43:14 -0500

    fix coloring when letters are duplicated

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import Grid from './Grid';
 import Keyboard from './Keyboard';
 import useEventListener from './useEventListener';
-import words from './words.json'
+import words from './words.json';
 
 const ALLOWED_GUESSES = 6
 const WORD_LENGTH = 5
@@ -13,7 +13,6 @@ const WORD_LENGTH = 5
 const dayOfYear = (date: Date) => Math.floor((date.valueOf() - new Date(date.getFullYear(), 0, 0).valueOf()) / 1000 / 60 / 60 / 24);
 
 export const ANSWER = words[dayOfYear(new Date()) % words.length]
-console.log(ANSWER)
 
 enum GameStatus {
   ONGOING,
@@ -29,7 +28,9 @@ enum LetterClasses {
 export const getLetterClass = (letter: string, index: number) => {
   if (!ANSWER.toLowerCase().includes(letter)) return LetterClasses.UNUSED
   else {
-    if (ANSWER.toLowerCase().split('').findIndex(l => l === letter) === index) return LetterClasses.CORRECT
+    const letters = ANSWER.toLowerCase().split('')
+    const letterIndices = letters.reduce((acc: Array<number>, char: string, i: number) => char === letter ? [...acc, i] : acc, [])
+    if (letterIndices.includes(index)) return LetterClasses.CORRECT
     return LetterClasses.CLOSE
   }
 }
@@ -40,20 +41,20 @@ function App() {
   const [currentGuess, setCurrentGuess] = useState('')
 
   const handleKeyDown = (key: string) => {
-    if(gameStatus === GameStatus.ONGOING) {
-        if (key === 'Backspace') {
-          setCurrentGuess(guess => guess.slice(0,-1))
-        }
-        else if (key === 'Enter' && currentGuess.length === WORD_LENGTH) {
-          validateGuess(currentGuess)
+    if (gameStatus === GameStatus.ONGOING) {
+      if (key === 'Backspace') {
+        setCurrentGuess(guess => guess.slice(0, -1))
+      }
+      else if (key === 'Enter' && currentGuess.length === WORD_LENGTH) {
+        validateGuess(currentGuess)
       }
       else if (currentGuess.length < WORD_LENGTH && key.length === 1 && key.match(/[a-zA-Z]/)) {
         setCurrentGuess(guess => (guess + key).toLowerCase())
       }
     }
   }
-  useEventListener('keydown', (event) => { 
-    if(event instanceof KeyboardEvent) handleKeyDown(event.key)
+  useEventListener('keydown', (event) => {
+    if (event instanceof KeyboardEvent) handleKeyDown(event.key)
   })
   const validateGuess = (guess: string) => {
     if (guess.toLowerCase() === ANSWER.toLowerCase()) {
@@ -63,7 +64,7 @@ function App() {
       alert('You won')
     }
     else if (guesses.length < ALLOWED_GUESSES) {
-      if (guesses.length === ALLOWED_GUESSES-1) {
+      if (guesses.length === ALLOWED_GUESSES - 1) {
         alert(`The word was: ${ANSWER}`)
         setGameStatus(GameStatus.GAME_OVER)
       }
@@ -81,7 +82,7 @@ function App() {
       </header>
       <main>
         <div className='grid-container'>
-          <Grid guesses={guesses} currentGuess={currentGuess}/>
+          <Grid guesses={guesses} currentGuess={currentGuess} />
         </div>
         <Keyboard onKeyDown={handleKeyDown} guesses={guesses} />
       </main>
